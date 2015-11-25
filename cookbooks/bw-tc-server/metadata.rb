4183d7ea06c65ed985cf5bb714f2b49c51d2415b
gonzalez <edds718@gmail.com>
2015-11-25 16:24:29 -0500

    clean up , prep for liferay tomcat server

--- cookbooks/bw-tc-server/metadata.rb
+++ cookbooks/bw-tc-server/metadata.rb
@@ -9,3 +9,4 @@ version          '0.1.0'
 depends 'marker'
 
 recipe 'bw-tc-server::install_tc_server', 'Installs TC Server 2.9'
+recipe 'bw-tc-server::install_liferay_server', 'Installs liferay tomcat server on tc server'
